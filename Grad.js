cf6bb7897786822d982c23246ad173c6a33a6170
GaryCUP <g.teague@eagle.clarion.edu>
2019-11-14 23:10:57 +0000

    g2

--- Grad.js
+++ Grad.js
@@ -3,9 +3,9 @@ window.onload = getTime()
 function getTime()
 {
     var date = new Date();
-    var current_hour = date.getHours();
+    var current_hour = date.getHours() ;
     var current_min = date.getMinutes();
     var current_sec=date.getSeconds();
-     window.alert(current_hour + ":" + current_min + "." +current_sec );
+     window.alert(current_hour, current_min , current_sec );
 }
 
