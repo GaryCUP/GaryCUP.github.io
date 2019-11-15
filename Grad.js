c709800653f0dae7ba75a1b6456e57972637a349
GaryCUP <g.teague@eagle.clarion.edu>
2019-11-15 00:23:30 +0000

    t9

--- Grad.js
+++ Grad.js
@@ -1,22 +1,21 @@
 function checkTime(i) {
-  if (i < 10) {
-    i = "0" + i;
-  }
-  return i;
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
 }
-function getTime()
-{
-    var date = new Date();
-    var current_hour = date.getHours() ;
-    var current_min = date.getMinutes();
-    var current_sec=date.getSeconds();
-     // add a zero in front of numbers<10
-  current_hour = checkTime(current_hour);
-  current_min = checkTime(current_min);
-  current_sec = checkTime(current_sec);
-  document.getElementById('time').innerHTML = current_hour + ":" + current_min + ":" + current_sec;
-  t = setTimeout(function() {
-    getTime()
-  }, 500)
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    // add a zero in front of numbers<10
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    t = setTimeout(function () {
+        startTime()
+    }, 500);
 }
-getTime();
+startTime();
