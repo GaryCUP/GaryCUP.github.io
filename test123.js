6b1e41fdaeb795331a11b5de25dbfb012bc1e58b
GaryCUP <47047840+GaryCUP@users.noreply.github.com>
2020-10-06 16:42:44 -0400

    Update test123.js

--- test123.js
+++ test123.js
@@ -14,11 +14,7 @@ function startTime() {
     // add a zero in front of numbers<10
     m = checkTime(m);
     s = checkTime(s);
-<<<<<<< HEAD:Grad.js
-    document.getElementById('time').innerHTML ="THE TIME IS"+ h + ":" + m + ":" + s;
-=======
     document.getElementById('timeis').innerHTML = h + ":" + m + ":" + s;
->>>>>>> 56dea9685d920a035ef3aa91389cf3bb898da693:test123.js
     t = setTimeout(function () {
         startTime()
     }, 500);
