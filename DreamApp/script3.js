0709340bbaa15ddf106315116e7bc6ca93ce3769
GaryCUP <47047840+GaryCUP@users.noreply.github.com>
2025-03-13 22:40:45 -0400

    Update script3.js

--- DreamApp/script3.js
+++ DreamApp/script3.js
@@ -75,7 +75,7 @@ document.addEventListener('DOMContentLoaded', function() {
     function parseCSV(csvText) {
         const lines = csvText.split('\n');
         const headers = lines[0].split(',');
-        return lines.slice(1).map(line => {
+        return lines.slice(1,lines.length-1).map(line => {
             const values = line.split(',');
             return headers.reduce((obj, header, index) => {
                 obj[header.trim()] = values[index] ? values[index].trim() : '';
